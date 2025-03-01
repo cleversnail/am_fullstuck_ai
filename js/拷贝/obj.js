996ce0427d201eec8df463e70168581725e13856
蜗牛 <652501825@qq.com>
2025-03-01 21:11:03 +0800

    aaa

---
+++ js/拷贝/obj.js
@@ -0,0 +1,17 @@
+Object.prototype.b = 2
+
+let obj = {
+  a: 1
+}
+
+console.log(obj.hasOwnProperty('b'));
+
+
+// obj.b = 2
+// let c = 'hello'
+// obj['c'] = 'world'
+// console.log(obj);
+
+// for (let key in obj) {
+//   console.log(key);
+// }
